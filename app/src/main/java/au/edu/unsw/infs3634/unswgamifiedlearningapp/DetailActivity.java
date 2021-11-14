c76f58e1b993050092f91b4db6ac9c6d313dd8c7
z5290731 <79190075+z5290731@users.noreply.github.com>
2021-11-14 22:56:19 +1100

    V.0.0.0.10
    BASIC DATA STRUCTURES
    LOGIN SCREEN (COMPLETED) BACKGROUND N LOGO REQUIRED
    QUIZ RIGHT Q LOGIC AND PASSING THROUGH DATA COMPLETED
    WORKING ON SCORE PAGE & DB PUSH
    PLANET LESSON PAGE FORMATTED (NEED TO ADD BACK AND HELP UI UX BUTTONS)
    PICTURES IMPORTED
    CODE BACKEND + FRONTEND XML READY FOR PARTS
    WIKI API COMPLETED (LINKING BETWEEN FRAGMENT + INTRO TO WIKI COMPLETED)

--- app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/DetailActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswgamifiedlearningapp/DetailActivity.java
@@ -5,29 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import au.edu.unsw.infs3634.unswgamifiedlearningapp.RecyclerAdapters.PlanetAdapter;
 import au.edu.unsw.infs3634.unswgamifiedlearningapp.uxui.journeyLog.journeyLogFragment;
-import au.edu.unsw.infs3634.unswgamifiedlearningapp.uxui.notesFragment;
 import au.edu.unsw.infs3634.unswgamifiedlearningapp.uxui.profile.cantinaFragment;
 import au.edu.unsw.infs3634.unswgamifiedlearningapp.uxui.spaceShip.spaceStationFragment;
 
 public class DetailActivity extends AppCompatActivity {
 
     public BottomNavigationView bottomNavigationView;
-    private static final String TAG = "TEST";
 
     private PlanetAdapter mPlanet;
 
@@ -40,51 +30,6 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navBar);
-
-        FirebaseAuth authentication = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = authentication.getCurrentUser();
-
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://spacefrontier-b2799-default-rtdb.asia-southeast1.firebasedatabase.app");
-        DatabaseReference name = database.getReference(FirebaseAuth.getInstance().getUid()).child("Name");
-        DatabaseReference XP = database.getReference(FirebaseAuth.getInstance().getUid()).child("XP");
-        DatabaseReference quizScore = database.getReference(FirebaseAuth.getInstance().getUid()).child("Score");
-
-
-        XP.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists() == false) {
-                    XP.setValue(0);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d(TAG, "Error: Consult FireBase Documentation for DB Issues");
-
-            }
-        });
-
-        quizScore.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists() == false) {
-                    quizScore.setValue(0);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d(TAG, "Error: Consult FireBase Documentation for DB Issues");
-
-            }
-        });
-
-        name.setValue(currentUser.getDisplayName());
-
-        System.out.println(String.valueOf(currentUser.getDisplayName()));
-
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -102,12 +47,6 @@ public class DetailActivity extends AppCompatActivity {
                         FragmentTransaction fragmentTransactionS = getSupportFragmentManager().beginTransaction();
                         fragmentTransactionS.replace(R.id.fragment_container, spaceStation, "FragmentName");
                         fragmentTransactionS.commit();
-                        return true;
-                    case R.id.mission_notes:
-                        notesFragment notes = new notesFragment();
-                        FragmentTransaction fragmentTransactionN = getSupportFragmentManager().beginTransaction();
-                        fragmentTransactionN.replace(R.id.fragment_container, notes, "FragmentName");
-                        fragmentTransactionN.commit();
                         return true;
                     case R.id.nav_cantina:
                         cantinaFragment cantina = new cantinaFragment();
